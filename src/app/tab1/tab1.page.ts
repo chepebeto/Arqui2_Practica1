5d85c4e3a424e06ab2fe0127ecd108e8500eb223
Jose Alberto Velasquez <josevelasquez3064@gmail.com>
2020-08-16 22:29:39 -0600

    Pantalla desinfectante funcionando

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { InformacionService } from './../services/informacion.service';
+import { Datos } from './../interfaces/datos';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  peso: number = 0;
+  estado_buzon: false;
+  datosR: Datos[] = [];
+  datosR2 = [
+    {
+      estado_buzon: false,
+      nivel_liquido: "Bajo",
+      percent_liquido: "15",
+      peso: 0
+    }
+  ];
+  constructor(private informacionService: InformacionService) {}
 
-  constructor() {}
+  ionViewWillEnter(){
+    this.informacionService.getDatos()
+    .subscribe(misdatos => {       
+      this.datosR = misdatos;
+      console.log(this.datosR);
+    });
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,20 +1,23 @@
 import { InformacionService } from './../services/informacion.service';
 import { Component } from '@angular/core';
-
+import { Datos } from './../interfaces/datos';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page{
+  semaforo: string= "Medio";
+  datosR: Datos[] = [];
+  
+  constructor(private informacionService: InformacionService) {}
 
-  constructor(private infoService: InformacionService) {}
-
-  getDatos(){
-    this.infoService.getDatos()
-    .subscribe(misdatos => {
-      console.log(misdatos);
+  ionViewWillEnter(){
+    this.informacionService.getDatos()
+    .subscribe(misdatos => {       
+      this.datosR = misdatos;
+      console.log(this.datosR);
     });
   }
 
