8eedfbb7497938d6cbbc17ebc0df494f2779e7e9
Jose Alberto Velasquez <josevelasquez3064@gmail.com>
2020-08-18 00:19:52 -0600

    Aplicación funcionando con alertas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { InformacionService } from './../services/informacion.service';
 import { Datos } from './../interfaces/datos';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -10,14 +11,30 @@ import { Datos } from './../interfaces/datos';
 export class Tab1Page {
   datosR: Datos[] = [];
   
-  constructor(private informacionService: InformacionService) {}
+  constructor(private informacionService: InformacionService, public alertController: AlertController) {}
 
   ionViewWillEnter(){
     this.informacionService.getDatos()
     .subscribe(misdatos => {       
-      this.datosR = misdatos;
-      console.log(this.datosR);
+      this.datosR = misdatos;      
+
+      let arr = Object.entries(this.datosR);
+
+      if ((arr[0][0]=="peso") && (arr[0][1].toString()!="0")){
+        this.AlertaPaqueteBuzon();
+      }
     });
+  }
+
+  async AlertaPaqueteBuzon() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: '¡Atención!',
+      subHeader: 'ACE2-APP',
+      message: 'Actualmente tiene un paquete en el buzón.!',
+      buttons: ['Entendido']
+    });
+    await alert.present();
   }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { InformacionService } from './../services/informacion.service';
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { Datos } from './../interfaces/datos';
 
 @Component({
@@ -10,14 +11,33 @@ import { Datos } from './../interfaces/datos';
 export class Tab2Page{  
   datosR: Datos[] = [];
   
-  constructor(private informacionService: InformacionService) {}
+  constructor(private informacionService: InformacionService, public alertController: AlertController) {}
 
   ionViewWillEnter(){
     this.informacionService.getDatos()
     .subscribe(misdatos => {       
-      this.datosR = misdatos;
-      console.log(this.datosR);
+      this.datosR = misdatos;     
+
+      let arr = Object.entries(this.datosR);
+      console.log(arr);
+
+      if ((arr[1][0]=="nivel_liquido") && (arr[1][1].toString()=="Bajo")){
+        this.AlertaNivelBajo();
+      }
+
+            
     });
   }
 
+  
+  async AlertaNivelBajo() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: '¡Atención!',
+      subHeader: 'ACE2-APP',
+      message: 'Nivel bajo de desinfectante!',
+      buttons: ['Entendido']
+    });
+    await alert.present();
+  }
 }
